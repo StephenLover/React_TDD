7cf0834940fdf58750ced1fdbf80e0210b33e377
Stephen Wang <paradoxstephen@gmail.com>
2019-07-14 14:49:02 +1000

    pass the unit test for save button function called in NewRestaurantForm Component

--- dist/main.1e43358e.js
+++ dist/main.1e43358e.js
@@ -22131,13 +22131,15 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 var NewRestaurantForm =
 /*#__PURE__*/
@@ -22145,19 +22147,49 @@ function (_Component) {
   _inherits(NewRestaurantForm, _Component);
 
   function NewRestaurantForm() {
+    var _getPrototypeOf2;
+
+    var _this;
+
     _classCallCheck(this, NewRestaurantForm);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(NewRestaurantForm).apply(this, arguments));
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(NewRestaurantForm)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "state", {
+      inputText: ''
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "handleTextChange", function (event) {
+      _this.setState({
+        inputText: event.target.value
+      });
+    });
+
+    _defineProperty(_assertThisInitialized(_assertThisInitialized(_this)), "handleSave", function () {
+      var inputText = _this.state.inputText;
+      var onSave = _this.props.onSave;
+      onSave(inputText);
+    });
+
+    return _this;
   }
 
   _createClass(NewRestaurantForm, [{
     key: "render",
     value: function render() {
+      var inputText = this.state.inputText;
       return _react.default.createElement("div", null, _react.default.createElement("input", {
         type: "text",
-        "data-test": "newRestaurantName"
+        "data-test": "newRestaurantName",
+        onChange: this.handleTextChange,
+        value: inputText
       }), _react.default.createElement("button", {
-        "data-test": "saveNewRestaurantButton"
+        "data-test": "saveNewRestaurantButton",
+        onClick: this.handleSave
       }, "Save"));
     }
   }]);
--- src/NewRestaurantForm.js
+++ src/NewRestaurantForm.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 
 export default class NewRestaurantForm extends Component {
+  state = { inputText: '' }
+
+  handleTextChange = (event) => {
+    this.setState({ inputText: event.target.value });
+  }
+
+  handleSave = () => {
+    const { inputText } = this.state;
+    const { onSave } = this.props;
+    onSave(inputText);
+  }
   render() {
+    const { inputText } = this.state;
     return (
       <div>
         <input
           type="text"
           data-test="newRestaurantName"
+          onChange={this.handleTextChange}
+          value={inputText}
         />
 
         <button
           data-test="saveNewRestaurantButton"
+          onClick={this.handleSave}
         >
           Save
         </button>
